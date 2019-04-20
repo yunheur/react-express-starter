ab1b3aa5e9f4a0d568f675b66f9a81f540d3309d
yunheur <yunheur@gmail.com>
2019-04-20 15:17:26 +0900

    set normalize.css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'Root';
 import * as serviceWorker from 'serviceWorker';
+import 'styles/base.scss';
 
 const render = () => {
   if(process.env.NODE_ENV === 'development') {
